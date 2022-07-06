f6cebf1c7c333b06f6fa6a5f02b3b776b79dbd07
student-ops <gogakuec@gmail.com>
2022-07-06 19:02:55 +0900

    edit

--- db-test.go
+++ db-test.go
@@ -3,7 +3,6 @@ package main
 import (
 	"database/sql"
 	"log"
-
 	_ "github.com/go-sql-driver/mysql"
 )
 
@@ -13,18 +12,29 @@ func main() {
 
 func connectOnly() {
 	// データベースのハンドルを取得する
-	db, err := sql.Open("mysql", "lakky1:lakky1@(localhost:3306)/db1")
+	db, err := sql.Open("mysql", "lakky1:lakky1@(localhost:3306)/test")
 	if err != nil {
 		// ここではエラーを返さない
 		log.Fatal(err)
 	}
 	defer db.Close()
-
-	// 実際に接続する
-	err = db.Ping()
+	
+	ins, err := db.Prepare("INSERT INTO user VALUES(?, ?)")
 	if err != nil {
 		log.Fatal(err)
-	} else {
-		log.Println("データベース接続完了")
 	}
+	defer ins.Close()
+
+	// SQLの実行
+	res, err := ins.Exec(2, "chuken")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// 結果の取得
+	lastInsertID, err := res.LastInsertId()
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println(lastInsertID)
 }
