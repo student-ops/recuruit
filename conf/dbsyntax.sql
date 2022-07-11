48ee9c0c09bf8f59784a17d61e1e5b583bc98313
student-ops <gogakuec@gmail.com>
2022-07-11 17:26:05 +0900

    restart

---
+++ conf/dbsyntax.sql
@@ -0,0 +1,2 @@
+CREATE TABLE M_user (user_id varchar(50), pass_word varchar (50),date_created datetime,date_updated datetime,date_deleted datetime);
+INSERT INTO M_user VALUES("lakky1","rakky1",now(),now(),null);
--- templates/user-registered.gtpl
+++
@@ -1,8 +0,0 @@
-<!DOCTYPE html>
-<html>
-<body>
-  <h2>登録結果の確認</h2>
-  <span>{{.result}}</span><br>
-  <button type=“button” onclick="location.href='./user-form'">入力画面に戻る</button>
-</body>
-</html>
