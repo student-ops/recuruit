402dd9d9716e6795b4366a2adbe51a694cc2f35e
student-ops <gogakuec@gmail.com>
2022-07-11 22:07:32 +0900

    edit

--- main.go
+++ main.go
@@ -8,14 +8,11 @@ import (
 		"database/sql"
 		_ "github.com/go-sql-driver/mysql"
 	*/
-	"fmt"
+
 	"net/http"
 )
 
 func main() {
 	http.HandleFunc("/", req_handler.tophandle)
 	http.ListenAndServe(":8080", nil)
-	fmt.Println("endlo")
-	http.HandleFunc("/", req_handler.tophandle)
-
 }
