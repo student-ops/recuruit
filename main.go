80f7e21fff072b4e2e3731469b6029322058401c
student-ops <gogakuec@gmail.com>
2022-07-12 11:54:22 +0900

    success

--- main.go
+++ main.go
@@ -8,17 +8,29 @@ import (
 		"database/sql"
 		_ "github.com/go-sql-driver/mysql"
 	*/
+	"html/template"
 	"net/http"
-	"test/req_handler"
 )
+func topHandler(w http.ResponseWriter, r *http.Request) {
+	t, err := template.ParseFiles("html/top.html")
+	if err != nil {
+		panic(err.Error())
+	}
+	if err := t.Execute(w, nil); err != nil {
+		panic(err.Error())
+	}
+}
+
 
 func main() {
 	mux := http.NewServeMux()
-	mux.HandleFunc("/static",tophandler.topHandler)
+	mux.HandleFunc("/",topHandler)
+
+
 
 	// サーバーの定義と呼び出し
 	server := &http.Server{
-		Addr: "0.0.0.0.:8080",
+		Addr: "0.0.0.0:8080",
 		Handler:mux,
 	}
 	server.ListenAndServe()
