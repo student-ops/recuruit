f5a0cd903556972f05fff34abc3913e77971d2fc
student-ops <gogakuec@gmail.com>
2022-07-13 19:17:30 +0900

    edit

--- main.go
+++ main.go
@@ -9,10 +9,13 @@ import (
 		_ "github.com/go-sql-driver/mysql"
 	*/
 	"net/http"
+	"test/req_handle"
 )
 func main() {
 	mux := http.NewServeMux()
-	mux.HandleFunc("/",b)
+	mux.HandleFunc("/",req_handle.Top)
+	mux.HandleFunc("/create_account",req_handle.CreateAccount)
+	mux.HandleFunc("/login",req_handle.Login)
 	// サーバーの定義と呼び出し
 	server := &http.Server{
 		Addr: "0.0.0.0:8080",
---
+++ req_handle/req_handle.go
@@ -0,0 +1,33 @@
+package req_handle
+
+import (
+	"net/http"
+	"text/template"
+)
+func Top(w http.ResponseWriter, r *http.Request) {
+	t, err := template.ParseFiles("html/top.html")
+	if err != nil {
+		panic(err.Error())
+	}
+	if err := t.Execute(w, nil); err != nil {
+		panic(err.Error())
+	}
+}
+func CreateAccount(w http.ResponseWriter, r *http.Request) {
+	t, err := template.ParseFiles("html/create_account.html")
+	if err != nil {
+		panic(err.Error())
+	}
+	if err := t.Execute(w, nil); err != nil {
+		panic(err.Error())
+	}
+}
+func Login(w http.ResponseWriter, r *http.Request) {
+	t, err := template.ParseFiles("html/CreateAccount.html")
+	if err != nil {
+		panic(err.Error())
+	}
+	if err := t.Execute(w, nil); err != nil {
+		panic(err.Error())
+	}
+}
