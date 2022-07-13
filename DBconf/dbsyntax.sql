28bfb9c0766c636aafe3627a7f60a14884652f7b
student-ops <gogakuec@gmail.com>
2022-07-14 08:25:24 +0900

    format

--- DBconf/dbsyntax.sql
+++
@@ -1,2 +0,0 @@
-CREATE TABLE M_user (user_id varchar(50),user_name VARCHAR(50), pass_word varchar (50),date_created datetime,date_updated datetime,date_deleted datetime);
-INSERT INTO M_user VALUES("lakky1","iraon7874","rakky1",now(),now(),null);
---
+++ conf/dbsyntax.sql
@@ -0,0 +1,9 @@
+CREATE TABLE M_user (
+    user_id varchar(50),
+    user_name VARCHAR(50),
+    pass_word VARCHAR(50),
+    date_created datetime,
+    date_updated datetime,
+    date_deleted datetime
+);
+INSERT INTO M_user VALUES("lakky1","iraon7874","rakky1",now(),now(),null);
