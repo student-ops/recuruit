cf90b8c375b44322090ad2876cfde005a50a0bb0
student-ops <gogakuec@gmail.com>
2022-07-29 18:40:59 +0900

    edit

--- conf/dbsyntax.sql
+++ conf/dbsyntax.sql
@@ -3,3 +3,6 @@ CREATE TABLE UserValues(
     password varchar(50),
     created timestamp
 );
+INSERT INTO UserValues values('hurry','kane',now());
+INSERT INTO UserValues values('moh','salah',now());
+INSERT INTO UserValues values('son','hunmin',now());
