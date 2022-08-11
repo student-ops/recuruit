82a468002f69544bdc73cfd83244fcf8291d730e
student-ops <gogakuec@gmail.com>
2022-08-11 19:41:40 +0900

    bata def

--- conf/dbsyntax.sql
+++ conf/dbsyntax.sql
@@ -1,5 +1,6 @@
-CREATE TABLE UserValues(
+CREATE TABLE  if not exists UserValues(
     userid varchar(50),
     password varchar(50),
     created timestamp
 );
+insert into uservalues values('fefefe','fefefe',now());
